022de955bd25d020365e81b135993728f81a16d5
lpfjustino <lpfjustino@gmail.com>
2017-04-29 19:45:12 -0300

    Spectrum normalization and subplotting implemented

--- t3.py
+++ t3.py
@@ -9,17 +9,36 @@ def fourier_analysis(filename, s, c, show=0):
     f = np.fromfile(filename, dtype=np.int32)
     g = apply_gaussian_windowing(f, s)
 
-    plot_signal(f)
-    plot_signal(g)
+    n = len(f)
 
+    nfs_f = abs(fft(f)) / (2*n)
+    nfs_g = abs(fft(g)) / (2*n)
+
+    plot_comparison(f, g, nfs_f, nfs_g)
 
 def apply_gaussian_windowing(f, s):
-    window = signal.gaussian(len(f), std=s)
+    n = len(f)
+    window = signal.gaussian(n, std=n/s)
     return f*window
 
 
-def plot_signal(f):
-    plt.plot(f)
+def plot_comparison(*plots, cols=2):
+    n = len(plots)
+    rows = math.ceil(n/cols)
+
+    _, ax_arr = plt.subplots(rows, cols)
+
+    for (i, plot) in enumerate(plots):
+        row = math.floor(i/cols)
+        col = i - (row * cols)
+
+        if n > cols: # More than 1 line, ax_arr is a matrix
+            ax_arr[row, col].ticklabel_format(style='sci', axis='both', scilimits=(0, 0))
+            ax_arr[row, col].plot(plot)
+        else: # ax_arr is an array
+            ax_arr[col].ticklabel_format(style='sci', axis='both', scilimits=(0, 0))
+            ax_arr[col].plot(plot)
+
     plt.show()
 
-fourier_analysis(filename="file", s=100, c=2)
+fourier_analysis(filename="sound1.bin", s=5, c=2, show=1)
